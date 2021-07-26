c9ba7b94c6818eceda8711eca9ce026bc086ffe7
annanadirashvili <annanadirashvili29@gmail.com>
2021-07-26 22:34:00 +0400

    added slick

--- script.js
+++ script.js
@@ -5,3 +5,35 @@ let togglebutton = document.getElementById('toggle-button')
 togglebutton.addEventListener('click', function(){
     navbarLinks.classList.toggle('active');
 })
+// test
+
+$(function(){
+    $('.multiple-items').slick({
+     infinite: true,
+     // რამდენი სურათი გამოჩნდეს
+     slidesToShow: 3,
+     // სლაიდის დროს რამდენი ელემნტით გადავიდეს 
+     slidesToScroll: 1,
+     // რომ გამოჩნდეს ღილაკები
+     arrows: true,
+     // რომ გამოჩნდეს ბურთულები
+     dots:true,
+     // responsiv-ის კოდი
+     responsive: [
+         {
+           breakpoint: 1100,
+           settings: {
+             slidesToShow: 2,
+             slidesToScroll: 1
+           }
+         },
+         {
+           breakpoint: 570,
+           settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1
+           }
+         }
+     ]
+   });    
+ });
